ddbdc3725e9b6b8f6ac21b9a63ea5a04755bcd66
Ariel <arielsepton1@gmail.com>
2020-04-22 17:45:18 +0300

    commit

---
+++ classes/refrigerators_list.py
@@ -0,0 +1,12 @@
+from classes.refrigerator import Refrigerator
+
+
+class Refrigerators:
+    def __init__(self):
+        self.refrigerators_list = []
+
+    def add_a_fridge(self, fridge: Refrigerator):
+        self.refrigerators_list.append(fridge)
+
+    def sort_refrigerators_by_space_left(self) -> []:
+        self.refrigerators_list.sort(key=lambda x: x.place_left_in_the_fridge(), reverse=True)
